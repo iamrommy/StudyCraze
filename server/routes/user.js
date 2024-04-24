2c8f1e3656c48cc315c121ab848e5d478494efd9
Ramanjot Singh <125717171+iamrommy@users.noreply.github.com>
2024-04-24 15:29:06 +0530

    Update user.js

--- server/routes/user.js
+++ server/routes/user.js
@@ -66,9 +66,7 @@ router.get("/googlelogin/callback", async(req, res)=>{
         expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
         secure: process.env.NODE_ENV === "development" ? false : true,
         httpOnly: process.env.NODE_ENV === "development" ? false : true,
-        sameSite: process.env.NODE_ENV === "development" ? false : "none",
-        domain: ".vercel.app",
-        path: "/"
+        sameSite: process.env.NODE_ENV === "development" ? false : "none"
     };
 
     res.cookie('token', token, options);
