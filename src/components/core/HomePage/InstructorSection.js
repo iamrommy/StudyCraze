b6d0d41bdc943d9da90f8b3ba958530d0006640a
Ramanjot Singh <ramanjotsingh8574@gmail.com>
2024-09-24 21:41:47 +0530

    demo commit 2

--- src/components/core/HomePage/InstructorSection.js
+++ src/components/core/HomePage/InstructorSection.js
@@ -21,7 +21,7 @@ const InstructorSection = () => {
             </h1>
 
             <p className="font-medium text-[16px] text-justify w-[90%] text-richblack-300">
-              Instructors from around the world teach millions of students on StudyCraze. We provide the tools and skills to teach what you love.
+              Instructors from around the world teach millions of students on StudyCraze. We provide the tools and skills to teach what you love
             </p>
 
             <div className="w-fit">
