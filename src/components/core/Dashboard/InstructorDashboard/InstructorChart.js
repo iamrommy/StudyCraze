d682dbcacb649c399cb002f07606607b1f5c10e2
Ramanjot Singh <ramanjotsingh8574@gmail.com>
2024-09-29 02:23:56 +0530

    updated piechart length

--- src/components/core/Dashboard/InstructorDashboard/InstructorChart.js
+++ src/components/core/Dashboard/InstructorDashboard/InstructorChart.js
@@ -74,7 +74,7 @@ export default function InstructorChart({ courses }) {
           Income
         </button>
       </div>
-      <div className="relative mx-auto aspect-square h-full w-full">
+      <div className="relative mx-auto aspect-square h-[80%] w-[80%]">
         {/* Render the Pie chart based on the selected chart */}
         <Pie
           data={currChart === "students" ? chartDataStudents : chartIncomeData}
