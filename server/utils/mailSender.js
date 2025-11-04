ba994813c227eb05da2b569715491edd4acffdaf
Rommy Arora <ramanjotsingh8574@gmail.com>
2025-11-05 01:00:02 +0530

    testing payment emails

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -2,15 +2,14 @@ const nodemailer = require("nodemailer");
 
 const mailSender = async (email, title, body)=>{
     // console.log(email, title, body);
+    console.log(process.env.MAIL_USER, process.env.MAIL_PASS ? 'Password Loaded' : 'No Password');
     try{
         let transporter = nodemailer.createTransport({
             host: process.env.MAIL_HOST,
+            secure: true,
             auth: {
                 user: process.env.MAIL_USER,
                 pass: process.env.MAIL_PASS
-            },
-            tls: {
-                rejectUnauthorized: false
             }
         })
         
