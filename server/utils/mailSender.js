f6e77d2cc840aab11975cb39f3b6c1041b02d88a
Rommy Arora <ramanjotsingh8574@gmail.com>
2025-11-05 02:25:52 +0530

    fixed mailsender

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -1,59 +1,30 @@
-const { Resend } = require('resend');
-
-const resend = new Resend(process.env.RESEND_API_KEY);
+const nodemailer = require("nodemailer");
 
 const mailSender = async (email, title, body) => {
+  console.log("Sending email to:", email);
   try {
-    const response = await resend.emails.send({
-      from: 'StudyCraze <onboarding@resend.dev>',
-      to: [email],
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
+      },
+    });
+
+    const info = await transporter.sendMail({
+      from: "StudyCraze <studycrazeinfo@gmail.com>",
+      to: email,
       subject: title,
       html: body,
     });
 
-    if (response?.error) {
-      console.error("Mail send error:", response?.error);
-      return null;
-    }
-
-    console.log("Mail sent successfully:", response);
-    return response;
-  } catch (err) {
-    console.error("Mail error:", err);
-    return null;
+    console.log("Mail sent:", info.response);
+    return info;
+  } catch (error) {
+    console.error("Mail error:", error.message);
   }
 };
 
 module.exports = mailSender;
-
-
-// const nodemailer = require("nodemailer");
-
-// const mailSender = async (email, title, body)=>{
-//     // console.log(email, title, body);
-//     console.log(process.env.MAIL_USER, process.env.MAIL_PASS ? 'Password Loaded' : 'No Password');
-//     try{
-//         let transporter = nodemailer.createTransport({
-//             host: process.env.MAIL_HOST,
-//             secure: true,
-//             auth: {
-//                 user: process.env.MAIL_USER,
-//                 pass: process.env.MAIL_PASS
-//             }
-//         })
-        
-//         let info = await transporter.sendMail({
-//             from: "StudyCraze - by Rommy",
-//             to: `${email}`,
-//             subject: `${title}`,
-//             html: `${body}`
-//         })
-//         console.log(info);
-//         return info;
-//     }
-//     catch(error){
-//         console.log(error.message);
-//     }
-// }
-
-// module.exports = mailSender;
